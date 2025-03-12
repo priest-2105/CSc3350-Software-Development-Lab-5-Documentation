87144b05f04b0a75a0df62a9430b7593ed3da519
priest-2105 <priest2105@gmail.com>
2025-03-12 03:01:33 +0100

    logo webp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import logo from "@/public/logo.webp"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Documentation Site",
-  description: "A documentation site with sidebar navigation",
-    generator: 'v0.dev'
+  description: "A documentation site",
 }
 
 export default function RootLayout({
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href={logo.src} type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
