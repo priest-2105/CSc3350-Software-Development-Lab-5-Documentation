03df1e0810a82ff673871101e3dafb07431b5438
priest-2105 <priest2105@gmail.com>
2025-03-12 01:39:08 +0100

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
@@ -79,4 +78,3 @@ const config = {
 } satisfies Config
 
 export default config
-
